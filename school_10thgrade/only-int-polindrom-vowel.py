1b194587b6113b7d96fab602d6f0f5cfb430d710
munwriter <glebvysokov3@gmail.com>
2023-09-11 19:53:51 +0500

    hw from 11.09.23

--- school_10thgrade/only-int-polindrom-vowel.py
+++ school_10thgrade/only-int-polindrom-vowel.py
@@ -2,12 +2,14 @@ import re
 
 
 def vowel_int_palindrome(data: list):
-    VOWELS = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+    VOWELS = {'a', 'e', 'i', 'y', 'o', 'u', 'A', 'E', 'I', 'Y', 'O', 'U'}
 
     palindrome = list(filter(lambda x: x == x[::-1], data))
     any_int = list(filter(lambda x: re.sub("\D", "", x) != '', data))
     any_vowel = list(filter(lambda x: any(i in VOWELS for i in x), data))
-    
+
     return any_vowel, any_int, palindrome
 
-assert vowel_int_palindrome(['123321', 'asdaxc1zxc', 'zxczxc', 'aaaaa']) == (['asdaxc1zxc', 'aaaaa'], ['123321', 'asdaxc1zxc'], ['123321', 'aaaaa'])
+
+assert vowel_int_palindrome(['123321', 'asdaxc1zxc', 'zxczxc', 'aaaaa']) == (
+    ['asdaxc1zxc', 'aaaaa'], ['123321', 'asdaxc1zxc'], ['123321', 'aaaaa'])
