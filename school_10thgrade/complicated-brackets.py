c28b641cee02f021cabe5988325d845cc833970e
munwriter <glebvysokov3@gmail.com>
2023-10-12 21:44:16 +0500

    complicated brackets

--- school_10thgrade/complicated-brackets.py
+++ school_10thgrade/complicated-brackets.py
@@ -1,29 +1,30 @@
-#in process...
-
-
-def complicated_brackets(brackets_string):
-    opened_brackets = ['(', '[', '{', '<']
-    closed_brackets = [')', ']', '}', '>']
-    brackets_shifts = [0, 0, 0, 0]
+def valid_complicated_brackets(brackets_string):
+    open_closed_brackets = {
+        '[': ']',
+        '{': '}',
+        '(': ')',
+        '<': '>',
+    }
+    # firs in|last out
+    stack = []
+    
     for bracket in brackets_string:
-        try:
-            i = opened_brackets.index(bracket)
-            brackets_shifts[i] += 1
-        except ValueError:
-            i = closed_brackets.index(bracket) 
-            brackets_shifts[i] -= 1
-        if sum(1 for n in brackets_shifts if n < 0) > 0:
+        if bracket in open_closed_brackets:
+            stack.append(bracket)
+        elif len(stack) == 0 or bracket != open_closed_brackets[stack[-1]]:
             return False
-    if sum(brackets_shifts) == 0:
-        return True
+        else:
+            stack.pop()
+    return len(stack) == 0
 
 
 def test_cases():
-    assert complicated_brackets('({[[((()))]]})[{{{<>}}}]') == True
-    assert complicated_brackets('({[}[((()))]]})[{{{<>}}}]') == False
-    assert complicated_brackets('(]') == False
-    assert complicated_brackets(')((())))') == False
-    assert complicated_brackets('[{{(})}]') == False
+    assert valid_complicated_brackets('({[[((()))]]})[{{{<>}}}]') == True
+    assert valid_complicated_brackets('({[}[((()))]]})[{{{<>}}}]') == False
+    assert valid_complicated_brackets('(]') == False
+    assert valid_complicated_brackets(')((())))') == False
+    assert valid_complicated_brackets('[{{(})}]') == False
 
- 
+
 test_cases()
+
