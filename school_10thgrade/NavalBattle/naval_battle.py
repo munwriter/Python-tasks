78310d34e78dbe1333b4c5f55f8a7dcaacd7b53d
munwriter <glebvysokov3@gmail.com>
2024-01-29 01:48:09 +0500

    realized system of defining ships

--- school_10thgrade/NavalBattle/naval_battle.py
+++ school_10thgrade/NavalBattle/naval_battle.py
@@ -6,9 +6,9 @@ from coordinates import Coordinates, LetterCoordinates
 
 class NavalBattle:
     def __init__(self, path: str | Path, coordinates_system: Coordinates) -> None:
-        self.flotilla: list[Coordinates] = []
         self.battlefield = self._load_field(path)
         self.__Cords = coordinates_system
+        self.flotilla = self._define_ships_positions(self.battlefield)
 
     def _load_field(self, path: str | Path) -> list[list[int]]:
         field = []
@@ -19,62 +19,52 @@ class NavalBattle:
 
         return field
 
-    def _define_ships_positions(self):
+    def _define_ships_positions(self, battlefield):
         # TODO find more optimized way
         CRITICAL_COORDS = (0, 9)  # TODO Enums maybe
         ship = None
-        saved_cords = set()
+        flotilla = []
         # traverse horizontally
         for row in range(10):
             for col in range(10):
-                if self.battlefield[row][col] == 1:
+                if battlefield[row][col] == 1:
                     # Checking for vertical ship
                     if row != CRITICAL_COORDS[0]:
-                        if self.battlefield[row - 1][col] == 1:
+                        if battlefield[row - 1][col] == 1:
                             continue
                     if row != CRITICAL_COORDS[1]:
-                        if self.battlefield[row + 1][col] == 1:
+                        if battlefield[row + 1][col] == 1:
                             continue
+                    battlefield[row][col] = 0
                     if ship is None:
                         ship = [self.__Cords(row, col)]
-                        saved_cords.add(self.__Cords(row, col).coordinates)
                     else:
                         ship.append(self.__Cords(row, col))
-                        saved_cords.add(self.__Cords(row, col).coordinates)
                     if col != CRITICAL_COORDS[1]:
-                        if self.battlefield[row][col + 1] == 0:
-                            self.flotilla.append(ship)
+                        if battlefield[row][col + 1] == 0:
+                            flotilla.append(ship)
                             ship = None
                     else:
-                        self.flotilla.append(ship)
+                        flotilla.append(ship)
                         ship = None
         # traverse vertically
         ship = None
 
         for col in range(10):
             for row in range(10):
-                if self.battlefield[row][col] == 1:
-                    # Checking for horizontal ship
-                    if col != CRITICAL_COORDS[0]:
-                        if self.battlefield[row][col - 1] == 1:
-                            continue
-                    if col != CRITICAL_COORDS[1]:
-                        if self.battlefield[row][col + 1] == 1:
-                            continue
-                    if (
-                        ship is None
-                        and self.__Cords(row, col).coordinates not in saved_cords
-                    ):
+                if battlefield[row][col] == 1:
+                    if ship is None:
                         ship = [self.__Cords(row, col)]
-                    elif self.__Cords(row, col).coordinates not in saved_cords:
+                    else:
                         ship.append(self.__Cords(row, col))
                     if row != CRITICAL_COORDS[1]:
-                        if self.battlefield[row + 1][col] == 0:
-                            self.flotilla.append(ship)
+                        if battlefield[row + 1][col] == 0:
+                            flotilla.append(ship)
                             ship = None
                     else:
-                        self.flotilla.append(ship)
+                        flotilla.append(ship)
                         ship = None
+        return flotilla
 
     def strike(
         self, x_position: int, y_position: int
@@ -83,4 +73,4 @@ class NavalBattle:
 
 
 a = NavalBattle(r"school_10thgrade\NavalBattle\battlefield.txt", LetterCoordinates)
-print(a._define_ships_positions(), a.flotilla)
+print(a.flotilla)
